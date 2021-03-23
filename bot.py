16d2fa001b644abdaba04232696b7e1f3119f751
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2021-03-23 19:29:43 +0000

    Add Ping Pong

--- bot.py
+++ bot.py
@@ -11,4 +11,7 @@ async def on_message(message):
     if message.content == 'Are you alive?':
         await message.channel.send('Yes, I am.')
 
+    if message.content == 'Ping!':
+        await message.channel.send(f'Pong! {round(client.latency * 1000)}ms')
+
 client.run(token)
